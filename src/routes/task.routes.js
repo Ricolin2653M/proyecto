2223e91e5c12b1fb92ea94ed175d27024846f23f
Carboc20 <114249065+Ricolin2653M@users.noreply.github.com>
2024-03-13 18:51:02 -0600

    si

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -9,6 +9,7 @@ import * as taskCtrl from '../controllers/task.controller';
 //Establecer las rutas de las tareas
 router.get('/', taskCtrl.getTask);
 router.post('/',  taskCtrl.createTask);
+//Rutas para el lider y admin
 router.get('/:taskId', taskCtrl.getTaskById);
 router.put('/:taskId', taskCtrl.updateTaskById);
 //Unicamente el admin elimina
