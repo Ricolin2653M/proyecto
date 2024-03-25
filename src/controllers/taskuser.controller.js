c12bd2a35a962a695b4f7c0b40c27060195a4f13
Carboc20 <114249065+Ricolin2653M@users.noreply.github.com>
2024-03-25 17:42:07 -0600

    h

--- src/controllers/taskuser.controller.js
+++ src/controllers/taskuser.controller.js
@@ -65,6 +65,7 @@ export const updateTaskUserById = async (req, res) => {
 
 // Función para eliminar una tarea por su ID
 /*
+
 export const deleteTaskById = async (req, res) => {
     try {
         const taskId = req.params.taskId; // Obtener el ID de la tarea desde los parámetros de la URL
