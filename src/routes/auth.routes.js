6105f6c5965d6f457584eed7fb5d4d4ae480c1b2
Ricolin2653M <car6920.03.03@gmail.com>
2024-06-02 19:00:34 -0600

    remove:"without token for register new user"

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,7 +4,7 @@ const router = Router();
 
 import * as authCtrl from "../controllers/auth.controller";
 
-router.post('/signup',[authJwt.verifyToken, authJwt.isLider], authCtrl.signUp)
+router.post('/signup', authCtrl.signUp)
 
 router.post('/signin', authCtrl.SignIn)
 
