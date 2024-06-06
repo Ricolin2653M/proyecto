b03fd3f7f55a92516c63755ae31a124453bd1227
Ricolin2653M <car6920.03.03@gmail.com>
2024-06-06 10:20:37 -0600

    updateuser

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,13 +4,15 @@ import User from "../models/User";
 export const getUserById = async (req, res) => {
     try {
         const userID = req.params.userID;
+        console.log("UserID:", userID);
         const user = await User.findById(userID);
 
         if (!user) {
-            return res.status(404).json({ message: ' Usuario no encontrado' });
+            return res.status(404).json({ message: 'Usuario no encontrado' });
         }
         res.status(200).json(user);
     } catch (error) {
+        console.error(error);
         res.status(500).json({ message: "Error al obtener el usuario" });
     }
 };
@@ -18,19 +20,23 @@ export const getUserById = async (req, res) => {
 // Editar usuario por ID
 export const updateUserById = async (req, res) => {
     try {
-        const UserID = req.params.UserID;
-        let { username, email } = req.body;
+        const userID = req.params.userID;
+        console.log("UserID:", userID);
+        const { username, email } = req.body;
 
         const updatedUser = await User.findByIdAndUpdate(
-            UserID,
+            userID,
             { username, email },
             { new: true }
         );
+
         if (!updatedUser) {
             return res.status(404).json({ message: "Usuario no encontrado" });
         }
-        res.status(200).json("Usuario actualizado");
+
+        res.status(200).json({ message: "Usuario actualizado", user: updatedUser });
     } catch (error) {
-        res.status(500).json({ message: "Error al actualizar" });
+        console.error(error);
+        res.status(500).json({ message: "Error al actualizar", error: error.message });
     }
 };
